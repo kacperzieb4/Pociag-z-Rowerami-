4adc16523d9828c0c91528387081a37bd7128acc
Kacper Zięba <167540203+kacperzieb4@users.noreply.github.com>
2025-02-24 00:13:12 +0100

    Obsługa błędów

--- projekt_SO/dane.h
+++ projekt_SO/dane.h
@@ -6,6 +6,6 @@
 #define R   10   // liczba rowerów maksymalnie w pociągu
 #define T   30  // co ile sekund pociąg musi odjechać 
 #define TI  30   // czas jazdy pociągu w jedną stronę w sekundach 
-#define TP  100  // liczba pasażerów generowanych w programie
+#define TP  50  // liczba pasażerów generowanych w programie
 
 #endif
--- projekt_SO/main.c
+++ projekt_SO/main.c
@@ -2,37 +2,59 @@
 #include "dane.h"
 
 void* start_kierownikow(void* arg) {
+    int created_trains = 0;
     for (int i = 0; i < N; i++) {
         pid_t kid = fork();
-        if (kid == 0) {
+        if (kid == -1) {
+            perror("fork kierownik");
+            continue;
+        } else if (kid == 0) {
             execl("./kierownik", "kierownik", NULL);
             perror("execl kierownik");
             exit(1);
         }
+        created_trains++;
+    }
+    if (created_trains != N) {
+        printf("\033[1;31m[MASTER] Liczba utworzonych procesów kierowników (%d) nie zgadza się z oczekiwaną wartością N (%d).\033[0m\n", created_trains, N);
     }
     return NULL;
 }
+
 void* start_pasazerowie(void* arg) {
+    int created_passengers = 0;
     for (int i = 0; i < TP; i++) {
         pid_t pid_p = fork();
-        if (pid_p == 0) {
+        if (pid_p == -1) {
+            perror("fork pasazer");
+            continue;
+        } else if (pid_p == 0) {
             execl("./pasazer", "pasazer", NULL);
             perror("execl pasazer");
             exit(1);
         }
+        created_passengers++;
         usleep((rand() % 2000 + 500) * 1000);
+    }
+    if (created_passengers != TP) {
+        printf("\033[1;31m[MASTER] Liczba utworzonych procesów pasażerów (%d) nie zgadza się z oczekiwaną wartością TP (%d).\033[0m\n", created_passengers, TP);
     }
     return NULL;
 }
+
+// Inicjalizacja pamięci dla liczby zabitych pasażerów
+int killed_passengers = 0;
+
 void* cleanup_passengers_main(void* arg) {
     int cleanup_msq = get_message_queue(".", 4);
     struct message msg;
 
-    while (1) {
+    while (killed_passengers < TP) {
         if (receive_message(cleanup_msq, 1, &msg) == 1) {
             pid_t passenger_pid = msg.ktype;
             if (kill(passenger_pid, SIGKILL) == 0) {
                 printf("\033[;35mPasażer PID=%d dojechał do celu.\033[0m\n", passenger_pid);
+                killed_passengers++;
                 waitpid(passenger_pid, NULL, 0);  // Zapobieganie zombie
             }
         }
@@ -40,6 +62,7 @@ void* cleanup_passengers_main(void* arg) {
     }
     return NULL;
 }
+
 void cleanup() {
     printf("\033[1;34m[MASTER] Sprzątanie zasobów.\033[0m\n");
 
@@ -57,12 +80,10 @@ void cleanup() {
     sem_destroy(sem_get(".", 5, 1)); // Peron
     sem_destroy(sem_get(".", 6, 1)); // Synchronizacja pociągów
 
-    //Usuwanie pamięci współdzielonej
-    shared_mem_destroy(shared_mem_get(".", 7));
-
     printf("\033[1;34m[MASTER] Wszystkie zasoby zostały usunięte.\033[0m\n");
     exit(0);
 }
+
 void sigint_handler_main(int sig) {
     printf("\n\033[1;34m[MASTER] SIGINT \033[0m\n");
     cleanup();
@@ -87,15 +108,13 @@ int main() {
         exit(1);
     }
     sem_set_value(next_train_sem, 0, 1);  // Następny pociąg może wjechać (wartość 1)
-    
-    // Inicjalizacja pamięci współdzielonej dla liczby zabitych pasażerów
-    int shm_id = shared_mem_create(".", 7, sizeof(int));
-    int *killed_passengers = shared_mem_attach_int(shm_id);
-    *killed_passengers = 0;  // Inicjalizacja licznika
 
     // Uruchomienie zawiadowcy
     pid_t zawiadowca_pid = fork();
-    if (zawiadowca_pid == 0) {
+    if (zawiadowca_pid == -1) {
+        perror("fork zawiadowca");
+        exit(1);
+    } else if (zawiadowca_pid == 0) {
         execl("./zawiadowca", "zawiadowca", NULL);
         perror("execl zawiadowca");
         exit(1);
@@ -104,19 +123,34 @@ int main() {
     sleep(1);  // Czekanie, aby zawiadowca się uruchomił
     pthread_t kierownicy_thread, pasazerowie_thread, cleanup_thread;
     // Tworzenie wątków do równoczesnego uruchomienia procesów
-    pthread_create(&kierownicy_thread, NULL, start_kierownikow, NULL);
-    pthread_create(&pasazerowie_thread, NULL, start_pasazerowie, NULL);
-    pthread_create(&cleanup_thread, NULL, cleanup_passengers_main, NULL);
-
+    if (pthread_create(&kierownicy_thread, NULL, start_kierownikow, NULL) != 0) {
+        perror("pthread_create kierownicy_thread");
+        exit(1);
+    }
+    if (pthread_create(&pasazerowie_thread, NULL, start_pasazerowie, NULL) != 0) {
+        perror("pthread_create pasazerowie_thread");
+        exit(1);
+    }
+    if (pthread_create(&cleanup_thread, NULL, cleanup_passengers_main, NULL) != 0) {
+        perror("pthread_create cleanup_thread");
+        exit(1);
+    }
 
     // Czekanie na zakończenie wątków
-    pthread_join(kierownicy_thread, NULL);
-    pthread_join(pasazerowie_thread, NULL);
+    if (pthread_join(kierownicy_thread, NULL) != 0) {
+        perror("pthread_join kierownicy_thread");
+    }
+    if (pthread_join(pasazerowie_thread, NULL) != 0) {
+        perror("pthread_join pasazerowie_thread");
+    }
 
     // Monitorowanie liczby zabitych(rozwiezionych) pasażerów
-    while (*killed_passengers < TP) {
+    while (killed_passengers < TP) {
         usleep(10000);
+    }
+    if (pthread_join(cleanup_thread, NULL) != 0) {
+        perror("pthread_join cleanup_thread");
     }
     printf("\033[1;34m[MASTER] Wszyscy pasazerowie dojechali do stacji końcowej. Kończę program.\033[0m\n");
     cleanup();
-} 
+}
